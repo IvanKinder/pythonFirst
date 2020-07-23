0479878842adb3c58e8730e7b61f7aa51611c2d4
Ivan Kinder <ivankinder11@gmail.com>
2020-07-23 16:17:13 +0300

    выполнил 6 задание

--- Урок 1. Практическое задание/task_5.py
+++ Урок 1. Практическое задание/task_5.py
@@ -1,39 +1,40 @@
 class StackClass:
-    def __init__(self, n):
+    def __init__(self, max_n):
         self.elements = [[]]
-        self.n = n
+        self.max_n = max_n
 
-    def is_empty(self):
+    def is_empty(self):  # пустой ли стек
         return self.elements == [[]]
 
-    def push_in(self, el):
-        if len(self.elements[-1]) < self.n:
+    def push_in(self, el):  # добавить элемент
+        if len(self.elements[-1]) < self.max_n:
             self.elements[-1].append(el)
         else:
             self.elements.append([el])
 
-    def pop_out(self):
+    def pop_out(self):  # убрать последний элемент элемент
         return self.elements[-1].pop()
 
-    def get_val(self):
+    def get_val(self):  # вернуть последний элемент
         return self.elements[-1][len(self.elements[-1]) - 1]
 
-    def stack_size(self):
+    def stack_size(self):  # общее количество тарелок
         return (len(self.elements) - 1) * len(self.elements[0]) + len(self.elements[-1])
 
-    def drop_last_stack(self):
+    def drop_last_stack(self):  # убрать последнюю стопку тарелок
         return self.elements.pop()
 
-    def drop_full_stack(self):
+    def drop_full_stack(self):  # убрать все тарелки
         self.elements = [[]]
 
 
-n = int(input('Введите пороговое значение для стопки тарелок: '))
-STACK_OBJ = StackClass(n)
+NUM = int(input('Введите пороговое значение для стопки тарелок: '))
+STACK_OBJ = StackClass(NUM)
 
 for i in range(10):
     STACK_OBJ.push_in(1)
 
-print(STACK_OBJ.elements, '\n', STACK_OBJ.get_val(), '\n', STACK_OBJ.stack_size(), '\n', STACK_OBJ.is_empty())
+print(STACK_OBJ.elements, '\n', STACK_OBJ.get_val(),
+      '\n', STACK_OBJ.stack_size(), '\n', STACK_OBJ.is_empty())
 STACK_OBJ.drop_full_stack()
 print(STACK_OBJ.elements)
--- Урок 1. Практическое задание/task_6.py
+++ Урок 1. Практическое задание/task_6.py
@@ -1,17 +1,41 @@
-"""
-Задание 7.
-Задание на закрепление навыков работы с очередью
+class QueueClass:
+    def __init__(self):
+        self.elements = []
+        self.done_tasks = []
 
-Примечание: в этом задании вспомните ваши знания по работе с ООП
-и опирайтесь на пример урока
+    def is_empty(self):  # пустая ли очередь
+        return self.elements == []
 
-Реализуйте структуру "доска задач".
+    def to_queue(self, item):  # добавить в очередь
+        self.elements.insert(0, item)
+
+    def from_queue(self):  # убрать из очереди
+        return self.elements.pop()
+
+    def size(self):  # размер очереди
+        return len(self.elements)
+
+    def done_task(self):  # добавить задачу в список выполненных
+        self.done_tasks.append(self.from_queue())
+
+    def to_rework(self, qu_obj):  # добавить задачу в очередь для корректировки
+        qu_obj.to_queue(self.from_queue())
 
 
-Структура должна предусматривать наличие несольких очередей задач, например
-1) базовой, откуда задачи берутся, решаются и отправляются в список решенных
-2) очередь на доработку, когда нерешенные задачи из первой очереди отправляются
-на корректировку решения
+BASE_TASKS = QueueClass()
+for i in range(10):  # наполняем очередь задачами
+    BASE_TASKS.to_queue('task_' + str(i))
+print('Всего задач: ', BASE_TASKS.elements)
 
-После реализации структуры, проверьте ее работу на различных сценариях
-"""
+for i in range(4):  # решаем несколько задач
+    BASE_TASKS.done_task()
+print('Осталось задач: ', BASE_TASKS.elements)
+print('Сделанные задачи: ', BASE_TASKS.done_tasks)
+
+REWORK_TASKS = QueueClass()  # создаем очередь для корректировочных задач
+
+BASE_TASKS.to_rework(REWORK_TASKS)  # отправляем пару заданий в очередь для корректировки
+BASE_TASKS.to_rework(REWORK_TASKS)
+
+print('Осталось задач: ', BASE_TASKS.elements)
+print('Задачи для доработки: ', REWORK_TASKS.elements)
