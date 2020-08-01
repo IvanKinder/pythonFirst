553225066923d97b2b6c2e9e6dc406228a02052f
Ivan Kinder <ivankinder11@gmail.com>
2020-08-01 17:50:52 +0300

    Сделал седьмое задание

--- task_7.py
+++ task_7.py
@@ -1,8 +1,21 @@
-"""
-7.	Напишите программу, доказывающую или проверяющую, что для множества
-натуральных чисел выполняется равенство: 1+2+...+n = n(n+1)/2,
- где n - любое натуральное число.
+STEP = 1
+PROGRESS_SUM = 0
 
- Решите через рекурсию. Решение через цикл не принимается.
-Для оценки Отлично в этом блоке необходимо выполнить 5 заданий из 7
-"""
+
+def sum_progression_check(n, step, progression_sum):
+    try:
+        if int(n) == 1:
+            return progression_sum + step
+        else:
+            progression_sum += step * int(n)
+            return sum_progression_check(int(n) - 1, step, progression_sum)
+    except RecursionError:
+        return 'Вы ввели либо ноль, либо cлишком большое число...'
+
+
+DIGITS_NUMBER = input('Введите натуральное число, при этом лучше не вводить больше 998: ')
+try:
+    print(sum_progression_check(DIGITS_NUMBER, STEP, PROGRESS_SUM))
+    print(f'Проверка по формуле n(n+1)/2:\n{int(DIGITS_NUMBER) * (int(DIGITS_NUMBER) + 1) / 2}')
+except ValueError:
+    print('Не надо вводить символы и буквы!!!')
