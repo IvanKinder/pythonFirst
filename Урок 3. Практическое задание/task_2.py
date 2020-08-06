d9b94e97f3e6b993ed919f6b7b876ff0218760bd
Ivan <ivankinder11@gmail.com>
2020-08-06 04:53:56 +0300

    Второе задание

--- Урок 3. Практическое задание/task_2.py
+++ Урок 3. Практическое задание/task_2.py
@@ -1,17 +1,27 @@
-"""
-Задание 2.
-Ваша программа должна запрашивать пароль
-Для этого пароля вам нужно получить хеш, используя функцию sha256
-Для генерации хеша обязательно нужно использовать криптографическую соль
-Обязательно выведите созданный хеш
+import hashlib
 
-Далее программа должна запросить пароль повторно
-Вам нужно проверить, совпадает ли пароль с исходным
-Для проверки необходимо сравнить хеши паролей
+pass_hash_data = {}
 
-ПРИМЕР:
-Введите пароль: 123
-В базе данных хранится строка: 555a3581d37993843efd4eba1921f1dcaeeafeb855965535d77c55782349444b
-Введите пароль еще раз для проверки: 123
-Вы ввели правильный пароль
-"""
+
+def login_program():
+    print('ДЛЯ ВЫХОДА ИЗ ПРОГРАММЫ ВВЕДИТЕ q!\n')
+    user_name = input('Введите имя: ')
+    if user_name.lower() == 'q':
+        print('Вы вышли из программы!')
+    else:
+        if user_name in pass_hash_data.keys():
+            user_pass = input('Пользователь существует! Введите пароль: ')
+            full_pass = user_name + user_pass
+            if hashlib.sha256(bytes(full_pass, encoding='utf-8')).hexdigest() == pass_hash_data[user_name].hexdigest():
+                print('Пароль введен правильно!\n')
+                login_program()
+            else:
+                print('Пароль введен неверно!!\n')
+                login_program()
+        else:
+            user_pass = input('Новый пользователь! Введите пароль: ')
+            pass_hash_data[user_name] = hashlib.sha256(bytes(user_name + user_pass, encoding='utf-8'))
+            login_program()
+
+
+login_program()
