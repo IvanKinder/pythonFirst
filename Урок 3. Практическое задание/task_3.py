571093f2fe4fbfd0fb85c906e207075b64e95d04
Ivan <ivankinder11@gmail.com>
2020-08-06 05:54:16 +0300

    Третье задание

--- Урок 3. Практическое задание/task_3.py
+++ Урок 3. Практическое задание/task_3.py
@@ -1,16 +1,25 @@
-"""
-Задание 3.
-Определить количество различных подстрок с использованием хеш-функции.
-Дана строка S длиной N, состоящая только из строчных латинских букв.
+from hashlib import sha256
 
-Подсказка: примените хеши и множества
+my_string = input('Введите строку!: ')
+my_list = []
+my_hash_set = set()
 
-рара:
 
-рар
-ра
-ар
-ара
-р
-а
-"""
+def under_strings(my_string_of_letters, my_l, number_of_letters):
+    if number_of_letters == len(my_string_of_letters):
+        return my_l
+    else:
+        for i in range(0, len(my_string_of_letters) + 1 - number_of_letters):
+            my_l.append(my_string_of_letters[i:(i + number_of_letters)])
+        return under_strings(my_string_of_letters, my_l, number_of_letters + 1)
+
+
+def make_hash():
+    for word in under_strings(my_string, my_list, 1):
+        my_hash_set.add(sha256(bytes(word, 'utf-8')).hexdigest())
+    print(my_hash_set)
+    print(set(my_list))
+    print(len(my_hash_set))
+
+
+make_hash()
