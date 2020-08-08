46510c010e4ef77e33bccb91491dc487c8338b52
Ivan Kinder <ivankinder11@gmail.com>
2020-08-08 17:43:03 +0300

    Первые штрихи

--- Урок 4. Практическое задание/task_1.py
+++ Урок 4. Практическое задание/task_1.py
@@ -1,13 +1,10 @@
 """
-Приведен код, который позволяет сохранить в
-массиве индексы четных элементов другого массива
-Сделайте замеры времени выполнения кода с помощью модуля timeit
-Попробуйте оптимизировать код, чтобы снизить время выполнения
-Проведите повторные замеры
-Добавьте аналитику: что вы сделали и почему
+Пробовал удалять нечетные элементы из имеющегося списка, но это не всегда было быстрее, а на больших списках медленнее
+Так тоже медленнее, но выглядит красивее
+Не смог придумать вариант быстрее, чем имеющийся
 """
 import timeit
-numbers = [12, 13, 11, 111, 43, 24, 22222, 2, 22, 77]
+numbers = [n for n in range(1000)]
 
 def func_1(nums):
     new_arr = []
@@ -17,13 +14,9 @@ def func_1(nums):
     return new_arr
 
 def func_2(nums):
-    new_arr = []
-    for i in range(len(nums)):
-        if nums[i] % 2 == 0:
-            new_arr.append(i)
-    return new_arr
+    return [nums.index(n) for n in nums if n % 2 == 0]
 
 print(f'Первая функция: {func_1(numbers)}')
 print(f'Вторая функция: {func_2(numbers)}')
-print(timeit.timeit(f'func_1({numbers})', setup='from __main__ import func_1', number=10000))
-print(timeit.timeit(f'func_2({numbers})', setup='from __main__ import func_2', number=10000))
+print(timeit.timeit(f'func_1({numbers})', setup='from __main__ import func_1', number=1))
+print(timeit.timeit(f'func_2({numbers})', setup='from __main__ import func_2', number=1))
