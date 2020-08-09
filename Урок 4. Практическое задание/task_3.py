2f3f659a19de925f0a8d6b6105be518f81928849
Ivan Kinder <ivankinder11@gmail.com>
2020-08-09 03:24:20 +0300

    Подправил условия третьей задачи

--- Урок 4. Практическое задание/task_3.py
+++ Урок 4. Практическое задание/task_3.py
@@ -1,17 +1,9 @@
 """
-Задание 3.
-
-Приведен код, формирующий из введенного числа
-обратное по порядку входящих в него
-цифр и вывести на экран.
-
 Сделайте профилировку каждого алгоритма через cProfile и через timeit
-
 Сделайте вывод, какая из трех реализаций эффективнее и почему
 """
-
-
-def revers(enter_num, revers_num=0):
+NUMBER = 123456789
+def revers(enter_num, revers_num=0):  # функция ничего не возвращает, исправил ниже
     if enter_num == 0:
         return
     else:
@@ -20,13 +12,22 @@ def revers(enter_num, revers_num=0):
         enter_num //= 10
         revers(enter_num, revers_num)
 
+def revers_1(enter_num, revers_num=0):
+    if enter_num == 0:
+        return int(revers_num)  # добавил здесь то, что возвращать
+    else:
+        num = enter_num % 10
+        revers_num = (revers_num + num / 10) * 10
+        enter_num //= 10
+        return revers_1(enter_num, revers_num)  # добавил здесь return
+
 
 def revers_2(enter_num, revers_num=0):
     while enter_num != 0:
         num = enter_num % 10
         revers_num = (revers_num + num / 10) * 10
         enter_num //= 10
-    return revers_num
+    return int(revers_num)  # добавил int чтоб было везде одинаково
 
 
 def revers_3(enter_num):
@@ -34,3 +35,7 @@ def revers_3(enter_num):
     revers_num = enter_num[::-1]
     return revers_num
 
+print(revers(NUMBER))  # видно, что функция ничего не возвращает
+print(revers_1(NUMBER))
+print(revers_2(NUMBER))
+print(revers_3(NUMBER))
