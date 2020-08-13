e83b22b60143aaf55601e3d83902f3e0229b0faf
Ivan Kinder <ivankinder11@gmail.com>
2020-08-13 03:16:10 +0300

    Кусок второго задания

--- Урок 5. Практическое задание/task_1.py
+++ Урок 5. Практическое задание/task_1.py
@@ -1,13 +1,15 @@
 from collections import deque
 
-
 COMPANIES = deque()
 
-for i in range(int(input('Введите количество предприятий для расчета прибыли: '))):
-    COMPANY = {}
-    comp_name = input(f'Введите название {i + 1} предприятия: ')
-    COMPANY[comp_name] = [float(input(f'Введите прибыль за {m + 1} квартал: ')) for m in range(4)]
-    COMPANIES.append(COMPANY)
+try:
+    for i in range(int(input('Введите количество предприятий для расчета прибыли: '))):
+        COMPANY = {}
+        comp_name = input(f'Введите название {i + 1} предприятия: ')
+        COMPANY[comp_name] = [float(input(f'Введите прибыль за {m + 1} квартал: ')) for m in range(4)]
+        COMPANIES.append(COMPANY)
+except ValueError:
+    print('Количество предприятий - это натуральное число, а прибыль - любое неотрицательное!\n')
 
 AVG_MONEY = sum([sum(max(i.values())) / len(COMPANIES) for i in COMPANIES])
 
@@ -22,6 +24,8 @@ for comp in COMPANIES:
             MORE_COMPS.add(key)
         elif sum(max(comp.values())) < AVG_MONEY:
             LESS_COMPS.add(key)
+        elif sum(max(comp.values())) == AVG_MONEY:
+            print('Прибыль каждой компании равна средней прибыли за год...')
 
 print(f'Предприятия с прибылью выше среднего: {MORE_COMPS}')
 print(f'Предприятия с прибылью ниже среднего: {LESS_COMPS}')
--- Урок 5. Практическое задание/task_2.py
+++ Урок 5. Практическое задание/task_2.py
@@ -10,6 +10,61 @@
 Также попробуйте решить задачу вообще без collections и применить только ваши знания по ООП
 (в частности по перегрузке методов)
 """
-class num16:
-    def __init__(self, *args):
-        
+from collections import defaultdict
+from collections import deque
+TRANS_DICT = {'a': 10, 'b': 11, 'c': 12, 'd': 13, 'e': 14, 'f': 15}
+class NUM16:
+
+    def __init__(self, letters):
+        self.n16 = letters[::-1]
+        self.trans_dict = {'a': 10, 'b': 11, 'c': 12, 'd': 13, 'e': 14, 'f': 15}
+        self.n2 = self.convert_to_2()
+
+    def convert_to_2(self):
+        converted = defaultdict(int)
+        con_nam = 0
+        for i in range(len(self.n16)):
+            if self.n16[i].isdigit():
+                converted[i] = int(self.n16[i])
+            else:
+                converted[i] = self.trans_dict[self.n16[i].lower()]
+        for key, value in converted.items():
+            con_nam += value*(16**key)
+        return con_nam
+
+
+    def __add__(self, other):
+        a = self.n2 + other.n2
+        return a
+
+    def __mul__(self, other):
+        pass
+
+def convert_to_16(a):
+    converted = deque()
+    converted2 = deque()
+    for key, value in TRANS_DICT.items():
+        if a == value:
+            return key
+    if a < 10:
+        return a
+    def recursion_convert(numb):
+        if numb < 16:
+            converted.appendleft(numb)
+            return converted
+        else:
+            converted.appendleft(numb % 16)
+            recursion_convert(numb // 16)
+    if a > 15:
+        recursion_convert(a)
+    for nu2 in converted:
+        if nu2 not in TRANS_DICT.values():
+            converted2.append(str(nu2))
+        else:
+            for key, value in TRANS_DICT.items():
+                if nu2 == value:
+                    converted2.append(key)
+    return converted2
+
+my16num = NUM16([j for j in 'F45ED23C'])
+print(''.join(convert_to_16(my16num + my16num)).upper())
