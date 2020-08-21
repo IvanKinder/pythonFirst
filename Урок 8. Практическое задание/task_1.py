7c71a38b690dccfc6dfeac714c4fe38d5f76aac1
Ivan Kinder <ivankinder11@gmail.com>
2020-08-21 11:14:54 +0300

    Сырое первое задание на троечку

--- Урок 8. Практическое задание/task_1.py
+++ Урок 8. Практическое задание/task_1.py
@@ -1,21 +1,48 @@
 """
-Реализуйте кодирование строки "по Хаффману".
-У вас два пути:
-1) тема идет тяжело? тогда вы можете, опираясь на пример с урока, сделать свою версию алгоритма
-Разрешается и приветствуется изменение имен переменных, выбор других коллекций, различные изменения
-и оптимизации.
-КОПИПАСТ ПРИМЕРА ПРИНИМАТЬСЯ НЕ БУДЕТ!
-2) тема понятна? постарайтесь сделать свою реализацию.
-Вы можете реализовать задачу, например, через ООП или предложить иной подход к решению.
+Сырой вариант по коду из лекции
 """
-MY_STRING = 'beep boop beer!'
-FREQ_DICT = {}
+from collections import Counter, deque
 
-for letter in MY_STRING:
-    FREQ_DICT[letter] = MY_STRING.count(letter)
 
-SORTED_VALUES_LIST = sorted(FREQ_DICT.values())
+def binary_haf(count_list):
+    freq_dict = Counter(count_list)
+    freq_list = deque(sorted(freq_dict.items(), key=lambda item: item[1]))
+    if len(freq_list) != 1:
+        while len(freq_list) > 1:
+            freq = freq_list[0][1] + freq_list[1][1]
+            knot = {0: freq_list.popleft()[0],
+                    1: freq_list.popleft()[0]}
 
-print(FREQ_DICT)
-print(SORTED_VALUES_LIST)
+            for i, new_freq in enumerate(freq_list):
+                if freq > new_freq[1]:
+                    continue
+                else:
+                    freq_list.insert(i, (knot, freq))
+                    break
+            else:
+                freq_list.append((knot, freq))
+    else:
+        freq = freq_list[0][1]
+        knot = {0: freq_list.popleft()[0]}
+        freq_list.append((knot, freq))
+    return freq_list[0][0]
 
+
+code_table = {}
+
+
+def code_haf(tree, path=''):
+    if type(tree) != dict:
+        code_table[tree] = path
+    else:
+        code_haf(tree[0], path=f'{path}0')
+        code_haf(tree[1], path=f'{path}1')
+
+
+MY_STRING = "beep boop beer!"
+
+code_haf(binary_haf(MY_STRING))
+
+for i in MY_STRING:
+    print(code_table[i], end=' ')
+print()
